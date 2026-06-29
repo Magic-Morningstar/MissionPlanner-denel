5888b7732282b75f038d9942ac8f421e163c7653
Taariq D Cassim <taariqdc@gmail.com>
2026-06-29 15:42:09 +0200

    Updated alert UI

--- Plugins/DenelPythonLauncher.cs
+++ Plugins/DenelPythonLauncher.cs
@@ -1,6 +1,7 @@
 using MissionPlanner.Plugin;
 using System;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -112,12 +113,112 @@ namespace MissionPlanner.Plugin
                     {
                         string msg = line.Substring("BUZZER_ALERT:".Length);
                         Console.WriteLine("[DenelPythonLauncher] Buzzer alert: " + msg);
-                        MainV2.instance.Invoke(new Action(() =>
-                            CustomMessageBox.Show(msg, "GCS Alert",
-                                MessageBoxButtons.OK, MessageBoxIcon.Warning)));
+                        MainV2.instance.Invoke(new Action(() => ShowBuzzerAlert(msg)));
                         writer.WriteLine("BUZZER_ACK");
                     }
                 }
+            }
+        }
+
+        private void ShowBuzzerAlert(string msg)
+        {
+            var cyan = Color.FromArgb(0x00, 0xBF, 0xFF);
+            var dark = Color.FromArgb(0x16, 0x18, 0x1A);
+
+            using (var frm = new Form())
+            {
+                frm.Size            = new Size(500, 280);
+                frm.FormBorderStyle = FormBorderStyle.None;
+                frm.BackColor       = cyan;   // 3px cyan border shows through
+                frm.TopMost         = true;
+                frm.StartPosition   = FormStartPosition.Manual;
+                frm.KeyPreview      = true;
+                frm.KeyDown        += (s, e) => { if (e.KeyCode == Keys.Escape) frm.Close(); };
+
+                Rectangle screen = Screen.PrimaryScreen.Bounds;
+                frm.Location = new Point(
+                    screen.X + (screen.Width  - frm.Width)  / 2,
+                    screen.Y + (screen.Height - frm.Height) / 2);
+
+                // Dark content panel inset 3px to reveal the cyan border all around
+                var content = new Panel
+                {
+                    BackColor = dark,
+                    Location  = new Point(3, 3),
+                    Size      = new Size(frm.Width - 6, frm.Height - 6)
+                };
+
+                // Cyan title strip across the top
+                var titleBar = new Panel
+                {
+                    BackColor = cyan,
+                    Location  = new Point(0, 0),
+                    Size      = new Size(content.Width, 38)
+                };
+                var titleLabel = new Label
+                {
+                    Text      = "GCS ALERT",
+                    ForeColor = dark,
+                    BackColor = Color.Transparent,
+                    Font      = new Font("Segoe UI", 12f, FontStyle.Bold),
+                    Location  = new Point(12, 0),
+                    Size      = new Size(titleBar.Width - 12, titleBar.Height),
+                    TextAlign = ContentAlignment.MiddleLeft
+                };
+                titleBar.Controls.Add(titleLabel);
+
+                var warnIcon = new PictureBox
+                {
+                    Image     = SystemIcons.Warning.ToBitmap(),
+                    SizeMode  = PictureBoxSizeMode.StretchImage,
+                    Size      = new Size(48, 48),
+                    Location  = new Point(14, 60),
+                    BackColor = Color.Transparent
+                };
+
+                var msgLabel = new Label
+                {
+                    Text      = msg,
+                    ForeColor = Color.White,
+                    BackColor = Color.Transparent,
+                    Font      = new Font("Segoe UI", 12f),
+                    Location  = new Point(74, 55),
+                    Size      = new Size(content.Width - 94, 140),
+                    AutoSize  = false
+                };
+
+                var btnOk = new Button
+                {
+                    Text      = "OK",
+                    Size      = new Size(120, 40),
+                    Location  = new Point((content.Width - 120) / 2, content.Height - 60),
+                    BackColor = cyan,
+                    ForeColor = dark,
+                    FlatStyle = FlatStyle.Flat,
+                    Font      = new Font("Segoe UI", 11f, FontStyle.Bold),
+                };
+                btnOk.FlatAppearance.BorderColor = cyan;
+                btnOk.Click += (s, e) => frm.Close();
+                frm.AcceptButton = btnOk;
+
+                // Flash border and title bar cyan ↔ red at 500ms
+                bool flashOn = false;
+                var alertRed  = Color.FromArgb(0xFF, 0x33, 0x00);
+                var flashTimer = new System.Windows.Forms.Timer { Interval = 500 };
+                flashTimer.Tick += (s, e) =>
+                {
+                    flashOn            = !flashOn;
+                    var c              = flashOn ? alertRed : cyan;
+                    frm.BackColor      = c;
+                    titleBar.BackColor = c;
+                    titleLabel.ForeColor = flashOn ? Color.White : dark;
+                };
+                flashTimer.Start();
+                frm.FormClosed += (s, e) => { flashTimer.Stop(); flashTimer.Dispose(); };
+
+                content.Controls.AddRange(new Control[] { titleBar, warnIcon, msgLabel, btnOk });
+                frm.Controls.Add(content);
+                frm.ShowDialog();
             }
         }
 
